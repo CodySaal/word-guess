b966239555d6d402a3ae7df2eef85776b7ea3520
Cody <110630101+CodySaal@users.noreply.github.com>
2022-09-19 21:25:09 -0500

    StartRound, gets random word, renders _ into DOM

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var startBtn = document.querySelector(".start-button")
+var wordBlanksEl = document.querySelector(".word-blanks")
+
+var words = ["jacascript", "variable", "function", "python", "localstorage", "timeout", "interval", "object", "vscode"]
+var word 
+// score ariable
+// timeLeft variable
+
+
+function renderCharacters(){
+    var str = ""
+    for (var i =0; i < word.length; i++){
+        // if we have guessed the character 
+            // add the character into str
+        // else
+            // add an _ into str
+            str += "_ "
+            console.log(str)
+    }
+    wordBlanksEl.textContent = str.trim()
+}
+
+function startRound(){
+    // get random word form words array
+    var randomIndex = Math.floor(Math.random() * words.length);
+    word = words[randomIndex];
+    renderCharacters();
+}
+
+startBtn.addEventListener("click", startRound)
